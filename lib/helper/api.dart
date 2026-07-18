23a8f06195be38eb4fc10557e5ddc1b3ce8d88de
Amal Nofal <amalnofall20@gmail.com>
2026-07-18 20:33:37 +0300

    refactor: enhance api helper and finalize get and post methods

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,15 +1,46 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<dynamic> get({required String url}) async {
-    http.Response response = await http.get(Uri.parse(url));
+  // --- GET METHOD ---
+  Future<dynamic> get({required String url, String? token}) async {
+    Map<String, String> headers = {};
+
+    if (token != null) {
+      headers.addAll({"Authorization": "Bearer $token"});
+    }
+
+    http.Response response = await http.get(Uri.parse(url), headers: headers);
 
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
-      throw Exception('unexpected Error ${response.statusCode}');
+      throw Exception('Error: ${response.statusCode}');
+    }
+  }
+
+  // --- POST METHOD ---
+  Future<dynamic> post({
+    required String url,
+    required dynamic body,
+    String? token,
+  }) async {
+    Map<String, String> headers = {};
+
+    if (token != null) {
+      headers.addAll({"Authorization": "Bearer $token"});
+    }
+
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Error ${response.statusCode}: ${response.body}');
     }
   }
 }
